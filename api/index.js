72e2fffdbf0c646753d2bb50b120b8c89ed90ff1
HarshdeepKaur9368 <132921603+HarshdeepKaur9368@users.noreply.github.com>
2024-04-26 14:52:38 +0530

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -3,14 +3,20 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.sign.js";
+
 
 mongoose.connect("mongodb+srv://harshdeepkaursdgi:harshdeep@mern-estate.nbnczmo.mongodb.net/mern-estate?retryWrites=true&w=majority&appName=mern-estate");
 
 const app=express();
 
-app.listen("3000",()=>{
-console.log("server is listening to 3000!!!");
+app.listen("3001",()=>{
+console.log("server is listening to 3001!!!");
 })
+
+app.use(express.json());
 
 //api route
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+
