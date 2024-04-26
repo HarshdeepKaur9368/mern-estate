cb2e49e991abd285ad620752fcd33f9503f198f8
HarshdeepKaur9368 <132921603+HarshdeepKaur9368@users.noreply.github.com>
2024-04-26 15:13:55 +0530

    Add files via upload
    created middleware

--- api/controllers/auth.control.js
+++ api/controllers/auth.control.js
@@ -1,11 +1,17 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signUp=async(req,res)=>{
+export const signUp=async(req,res,next)=>{
     
 const{username,email,password}=req.body;
 const hashedPassword=bcryptjs.hashSync(password,10);
 const newUser=new User({username,email,password:hashedPassword});
-await newUser.save();
-res.status(201).json("USer created successfully");
+try{
+    await newUser.save();
+    res.status(201).json("USer created successfully");
+}catch(error){
+    next(errorHandler(550,"error from function"));
+}
+
 }
